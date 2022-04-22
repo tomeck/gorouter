c5c4beccefd4593c18f529e76000d1eefceda258
tomeck <dr.teck@gmail.com>
2022-04-22 11:27:00 -0400

    checkpoint

--- gorouter.go
+++ gorouter.go
@@ -12,7 +12,6 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	//	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
